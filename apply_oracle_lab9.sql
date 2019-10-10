efef3873f7ba383b5af1b27aeb8d2d58b4f1f73b
tsheffet <38865807+tsheffet@users.noreply.github.com>
2019-10-10 02:50:53 -0600

    Update apply_oracle_lab9.sql

--- apply_oracle_lab9.sql
+++ apply_oracle_lab9.sql
@@ -1,9 +1,4 @@
 -- ------------------------------------------------------------------
---  Program Name:   apply_oracle_lab9.sql
---  Lab Assignment: Lab #9
---  Program Author: Michael McLaughlin
---  Creation Date:  02-Mar-2010
--- ------------------------------------------------------------------
 -- Instructions:
 -- ------------------------------------------------------------------
 -- The two scripts contain spooling commands, which is why there
